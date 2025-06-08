402b51e083d4b5f3fce1138201dd0d5329398dbb
JunYoung <pde4735@naver.com>
2025-06-08 17:27:07 +0900

    Set: .gitignore 설정
    .

---
+++ src/main/java/com/example/ConcertTracker/ConcertTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConcertTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ConcertTracker/ConcertTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConcertTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
