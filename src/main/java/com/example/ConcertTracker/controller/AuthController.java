ff5d0427653e3648ca87f69540eab3faedbfdcc6
JunYoung <pde4735@naver.com>
2025-06-10 23:00:50 +0900

    Fix: AuthController, responsing Token
    .

--- src/main/java/com/example/ConcertTracker/controller/AuthController.java
+++ src/main/java/com/example/ConcertTracker/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.ConcertTracker.controller;
 
+import com.example.ConcertTracker.dto.AccessRefreshTokenResponseDto;
 import com.example.ConcertTracker.dto.kakaoAuthDto.AccessTokenResponseDto;
 import com.example.ConcertTracker.dto.kakaoAuthDto.UserInfoResponseDto;
 import com.example.ConcertTracker.service.kakaoAuthService;
@@ -11,10 +12,10 @@ public class AuthController {
     private kakaoAuthService kakaoAuthService;
 
     @PostMapping("/login/kakao")
-    public String authorize(@RequestHeader String authorization_code) { // Get Authorization
+    public AccessRefreshTokenResponseDto authorize(@RequestHeader String authorization_code) { // Get Authorization
         AccessTokenResponseDto AccessToken = kakaoAuthService.kakaoAuthorize(authorization_code); // send Authorization to service
         ResponseEntity<UserInfoResponseDto> userInfo = kakaoAuthService.kakaoGetUserInfo(AccessToken);
-        kakaoAuthService.checkingUserInfo(userInfo);
-        return "개발";
+        AccessRefreshTokenResponseDto accessRefreshTokenResponseDto = kakaoAuthService.authWithToken(userInfo);
+        return accessRefreshTokenResponseDto;
     }
 }
