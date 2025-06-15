958eb6697eaa01c7608fee4c5fd06b157e9ebe8c
JunYoung <pde4735@naver.com>
2025-06-15 16:58:55 +0900

    Readability
    .

--- src/main/java/com/example/oauthjwtweb/oauth_jwt_webApplication.java
+++ src/main/java/com/example/oauthjwtweb/oauth_jwt_webApplication.java
@@ -4,10 +4,4 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class oauth_jwt_webApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(oauth_jwt_webApplication.class, args);
-	}
-
-}
+public class oauth_jwt_webApplication { public static void main(String[] args) {SpringApplication.run(oauth_jwt_webApplication.class, args);}}
