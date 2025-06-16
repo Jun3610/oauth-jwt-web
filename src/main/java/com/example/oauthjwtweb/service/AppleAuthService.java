f6e6077708da6884b67f0ea080f76dba2ffed626
JunYoung <pde4735@naver.com>
2025-06-16 19:12:23 +0900

    Last Commit
    .

--- src/main/java/com/example/oauthjwtweb/service/AppleAuthService.java
+++ src/main/java/com/example/oauthjwtweb/service/AppleAuthService.java
@@ -1,9 +1,38 @@
 package com.example.oauthjwtweb.service;
 
+import com.example.oauthjwtweb.repository.NaverAuthRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Service
 public class AppleAuthService {
 
+    final private SecurityExpressionHandler<FilterInvocation> webSecurityExpressionHandler;
+    final private NaverAuthRepository naverAuthRepository;
+    final private RestTemplate restTemplate;
+    final private JwtService jwtService;
+
+    public AppleAuthService(SecurityExpressionHandler<FilterInvocation> webSecurityExpressionHandler,
+                            NaverAuthRepository naverAuthRepository,
+                            RestTemplate restTemplate,
+                            JwtService jwtService
+    ) {
+        this.webSecurityExpressionHandler = webSecurityExpressionHandler;
+        this.naverAuthRepository = naverAuthRepository;
+        this.restTemplate = restTemplate;
+        this.jwtService = jwtService;
+    }
+
+    public String setAppleAuthUrl(HttpSession session) {
+        session.setAttribute("state", UUID.randomUUID().toString());
+        return "https://appleid.apple.com/auth/authorize"
+                "&client_id" + 
+
+    }
 
 }
