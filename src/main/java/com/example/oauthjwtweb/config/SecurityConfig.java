0fc2ac51b5f80714c6949d62d30a78a1069c725e
JunYoung <pde4735@naver.com>
2025-06-17 02:48:38 +0900

    Study Spring Security
    .

--- src/main/java/com/example/oauthjwtweb/config/SecurityConfig.java
+++ src/main/java/com/example/oauthjwtweb/config/SecurityConfig.java
@@ -5,15 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@Configuration //Setting File
 public class SecurityConfig {
 
-    @Bean
+    @Bean // Be registered as Spring Bean: Spring create and manage this Object(this method)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-
+            http.csrf(csrf -> csrf.disable() )
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) ;
         return http.build();
     }
 }
