71c7cb21359cb436ce6f9840f50dab4deb0ad3b8
JunYoung <pde4735@naver.com>
2025-06-19 17:04:36 +0900

    Security Setting
    .

--- src/main/java/com/example/oauthjwtweb/config/SecurityConfig.java
+++ src/main/java/com/example/oauthjwtweb/config/SecurityConfig.java
@@ -1,21 +1,26 @@
 package com.example.oauthjwtweb.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //Setting File
 public class SecurityConfig {
+    private final CorsConfig corsConfig;
+
+    @Autowired
+    public SecurityConfig(CorsConfig corsConfig) {this.corsConfig = corsConfig;}
 
     @Bean // Be registered as Spring Bean: Spring create and manage this Object(this method)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf( (CsrfConfigurer<HttpSecurity> csrfConsumer) -> { csrfConsumer.disable(); })
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .cors( cors -> {});
+                .csrf( (CsrfConfigurer<HttpSecurity> csrfConsumer) -> {csrfConsumer.disable();} )
+                .cors((CorsConfigurer<HttpSecurity> corsConsumer) -> {corsConsumer.configurationSource(corsConfig.corsFilter());} )
+                .authorizeHttpRequests( auth -> auth.anyRequest().permitAll());
 
         return http.build();
     }
