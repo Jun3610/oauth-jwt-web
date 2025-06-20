7e796d5fe6f0ba1188b0db91189dd4f2da939370
JunYoung <pde4735@naver.com>
2025-06-20 15:13:03 +0900

    Admin Setting
    .

--- src/main/java/com/example/oauthjwtweb/controller/AdminController.java
+++ src/main/java/com/example/oauthjwtweb/controller/AdminController.java
@@ -1,7 +1,7 @@
 package com.example.oauthjwtweb.controller;
 
 import com.example.oauthjwtweb.entity.User;
-import com.example.oauthjwtweb.service.UserServiceDeveloper; // 또는 UserService 인터페이스
+import com.example.oauthjwtweb.service.AdminService; // 또는 UserService 인터페이스
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;  // 스프링 Model 임포트
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +13,9 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final UserServiceDeveloper userService;  // UserServiceDeveloper 사용
+    private final AdminService userService;  // UserServiceDeveloper 사용
 
-    public AdminController(UserServiceDeveloper userService) {
+    public AdminController(AdminService userService) {
         this.userService = userService;
     }
 
--- src/main/java/com/example/oauthjwtweb/service/AdminService.java
+++ src/main/java/com/example/oauthjwtweb/service/AdminService.java
@@ -7,16 +7,14 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class UserServiceDeveloper {
+public class AdminService {
 
     private final UserRepository userRepository;
 
-    // 생성자 주입
-    public UserServiceDeveloper(UserRepository userRepository) {
+    public AdminService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    // 사용자 전체 조회 메서드
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
